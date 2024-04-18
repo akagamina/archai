b0549074c95f9df5d916fd6ab9f9c467c23a44e5
akagamina <omer@gami.gg>
2024-04-19 02:14:14 +0300

    Refactor WebcamCapture component to remove unused code and improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 
-import React, { useRef, useState } from "react";
-import Webcam from "react-webcam";
-import { storage } from "../../firebase-config";
-import uploadImageAndGetDesignSuggestions from "@/services/gpt4Service";
-import { useUser } from "@/context/UserContext";
-import { CameraIcon, PhotoIcon } from "@heroicons/react/16/solid";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function WebcamCapture() {
-  const { setLoading } = useUser();
-  const webcamRef = React.useRef<Webcam>(null);
-  const [image, setImage] = useState<any>();
-
-  const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot();
-    setImage(imageSrc ?? null);
-  }, [webcamRef]);
-  const { user } = useUser();
-
-  const uploadImage = async () => {
-    setLoading(true);
-    try {
-      const imageBlob = await (await fetch(image)).blob();
-
-      const imageFile = new File([imageBlob], `image/${user?.uid}`, {
-        type: "image/png",
-      });
-      const suggestions: any = await uploadImageAndGetDesignSuggestions(
-        imageFile
-      );
-      window.open(suggestions?.data[0].url, "_blank");
-      setLoading(false);
-    } catch (error) {
-      console.error("Error in uploadImage: ", error);
-      setLoading(false);
-    }
-  };
-
+function Home() {
   return (
     <main className="main-wrapper relative overflow-hidden">
       <section id="hero-section">
@@ -391,4 +357,4 @@ function WebcamCapture() {
   );
 }
 
-export default WebcamCapture;
+export default Home;
